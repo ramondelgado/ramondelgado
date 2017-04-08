692133ba50b5e985ab68e0ac0ba71a0c29814cec
Ramon A. Delgado <ramon.delgado@uon.edu.au>
2017-04-08 16:11:34 +1000

    Add Arduino smart car project

--- includes/footer.js
+++ includes/footer.js
@@ -1,3 +1,3 @@
 document.write("<div id='footer'>");
-document.write(" &copy  2015 <a href='mailto:ramon.delgado_at_uon.edu.au'>RADP</a>|Design by <a href='#'>super j man</a> ");
+document.write(" &copy  2017 <a href='mailto:ramon.delgado_at_uon.edu.au'>RADP</a>");
 document.write("</div>");
--- includes/menu.js
+++ includes/menu.js
@@ -4,4 +4,5 @@ document.write(" <a href='index.html' title='Home'><span>Home</span></a> ");
 document.write("<a href='publications.html' title='Publications'><span>Publications</span></a>");
 document.write(" <a href='downloads.html' title='Downloads'><span>Downloads</span></a>");
 document.write(" <a href='useful.html' title='Useful'><span>Useful</span></a>");
+document.write(" <a href='hobbies.html' title='Hobbies'><span>Hobbies</span></a>");
 document.write(" </div>");
