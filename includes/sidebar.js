ee3459eca9167802276650593188b7b414d67247
Ramon Delgado <ramon.delgado@uon.edu.au>
2017-06-29 11:44:16 +1000

    Add favicon, links to other profiles such as researchgate, scopus and university of newcastle. Also, I added the paper in VTC2017-Fall to the publications.

--- includes/sidebar.js
+++ includes/sidebar.js
@@ -1,6 +1,9 @@
 document.write("<div id='sidebar'>");
 document.write("	<h3>Profiles</h3>");
+document.write("<a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a><br>");
 document.write("<a href='https://www.linkedin.com/in/ram%C3%B3n-delgado-038b5388/'>Linkedin</a><br>");
+document.write("<a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a><br>");
+document.write("<a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a><br>");
 document.write("<a ");
 document.write("  href='http://scholar.google.com.au/citations?user=L9US5bgAAAAJ&hl=en'");
 document.write("  target='_blank'>Google Scholar</a><br>");
