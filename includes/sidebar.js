588740b40cc990907200f36ff8255f044b0f598a
Ramon Delgado <ramon.delgado@uon.edu.au>
2017-07-11 17:52:11 +1000

    I fixed css and included a summary in publications

--- includes/sidebar.js
+++ includes/sidebar.js
@@ -1,14 +1,13 @@
 document.write("<div id='sidebar'>");
 document.write("	<h3>Profiles</h3>");
-document.write("<a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a><br>");
-document.write("<a href='https://www.linkedin.com/in/ram%C3%B3n-delgado-038b5388/'>Linkedin</a><br>");
-document.write("<a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a><br>");
-document.write("<a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a><br>");
-document.write("<a ");
+document.write("<p><a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a></p>");
+document.write("<p><a href='https://www.linkedin.com/in/ram%C3%B3n-delgado-038b5388/'>Linkedin</a></p>");
+document.write("<p><a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a></p>");
+document.write("<p><a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a></p>");
+document.write("<p><a ");
 document.write("  href='http://scholar.google.com.au/citations?user=L9US5bgAAAAJ&hl=en'");
-document.write("  target='_blank'>Google Scholar</a><br>");
-document.write("<a href='https://github.com/ramondelgado'>GitHub</a>");
-document.write("	<br><hr><br>");
-document.write("<br><img src='ramon_bio.jpg' alt='Right Align' height='112'class='center' />");
+document.write("  target='_blank'>Google Scholar</a></p>");
+document.write("<p><a href='https://github.com/ramondelgado'>GitHub</a></p>");
+document.write("<p><img src='ramon_bio.jpg' alt='Right Align' height='112'class='center' /></p>");
 document.write("</div>");
 
