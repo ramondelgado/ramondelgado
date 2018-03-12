6ab657c3171f41323aaf2100d7152eb1edfe901b
Ramon Delgado <ramon.delgado@uon.edu.au>
2018-03-12 13:04:50 +1100

    Added link to ORCID in the side bar. Ai also updated the publication list.

--- includes/sidebar.js
+++ includes/sidebar.js
@@ -1,13 +1,14 @@
 document.write("<div id='sidebar'>");
 document.write("	<h3>Profiles</h3>");
 document.write("<p><a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a></p>");
-document.write("<p><a href='https://www.linkedin.com/in/ram%C3%B3n-delgado-038b5388/'>Linkedin</a></p>");
+document.write("<p><a href='https://www.linkedin.com/in/ramon-delgado-038b5388/'>Linkedin</a></p>");
 document.write("<p><a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a></p>");
 document.write("<p><a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a></p>");
 document.write("<p><a ");
 document.write("  href='http://scholar.google.com.au/citations?user=L9US5bgAAAAJ&hl=en'");
 document.write("  target='_blank'>Google Scholar</a></p>");
 document.write("<p><a href='https://github.com/ramondelgado'>GitHub</a></p>");
+document.write("<a href='https://orcid.org/0000-0003-4313-2209' target='orcid.widget' rel='noopener noreferrer' style='vertical-align:top;'>ORCID</a>")
 document.write("<p><img src='ramon_bio2.jpg' alt='Right Align' height='112'class='center' /></p>");
 document.write("</div>");
 
