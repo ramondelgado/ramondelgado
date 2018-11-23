b103100e48678a05ea524ade3738c7108a2aece0
Ramon Delgado <ramon.delgado@uon.edu.au>
2018-11-23 16:38:44 +1100

    Added researcherID to side bar.

--- includes/sidebar.js
+++ includes/sidebar.js
@@ -4,6 +4,7 @@ document.write("<p><a href='https://www.newcastle.edu.au/profile/ramon-delgado'>
 document.write("<p><a href='https://www.linkedin.com/in/ramon-delgado-038b5388/'>Linkedin</a></p>");
 document.write("<p><a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a></p>");
 document.write("<p><a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a></p>");
+document.write("<p><a href='http://www.researcherid.com/rid/W-3643-2018'>ResearcherID</a></p>");
 document.write("<p><a ");
 document.write("  href='http://scholar.google.com.au/citations?user=L9US5bgAAAAJ&hl=en'");
 document.write("  target='_blank'>Google Scholar</a></p>");
