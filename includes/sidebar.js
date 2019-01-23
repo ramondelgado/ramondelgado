e08d455824fd91be2f7af0f784364b11c5d0ed9c
Ramon Delgado <ramon.delgado@uon.edu.au>
2019-01-23 12:00:03 +1100

    I added the VTM paper to the publication list, removed the link to scopus profile and updated the news in the index page.

--- includes/sidebar.js
+++ includes/sidebar.js
@@ -3,7 +3,7 @@ document.write("	<h3>Profiles</h3>");
 document.write("<p><a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a></p>");
 document.write("<p><a href='https://www.linkedin.com/in/ramon-delgado-038b5388/'>Linkedin</a></p>");
 document.write("<p><a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a></p>");
-document.write("<p><a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a></p>");
+//document.write("<p><a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a></p>");
 document.write("<p><a href='http://www.researcherid.com/rid/W-3643-2018'>ResearcherID</a></p>");
 document.write("<p><a ");
 document.write("  href='http://scholar.google.com.au/citations?user=L9US5bgAAAAJ&hl=en'");
