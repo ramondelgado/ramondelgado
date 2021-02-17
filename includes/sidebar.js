cf31ba7e7df507cbabbd34b7fc975ad509e3710c
Ramon Delgado <ramon.delgado@uon.edu.au>
2021-02-17 11:56:39 +1100

    Updated side bar

--- includes/sidebar.js
+++ includes/sidebar.js
@@ -1,10 +1,10 @@
 document.write("<div id='sidebar'>");
 document.write("	<h3>Profiles</h3>");
-document.write("<p><a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a></p>");
+//document.write("<p><a href='https://www.newcastle.edu.au/profile/ramon-delgado'>Uni. of Newcastle</a></p>");
 document.write("<p><a href='https://www.linkedin.com/in/ramon-delgado-038b5388/'>Linkedin</a></p>");
 document.write("<p><a href='https://www.researchgate.net/profile/Ramon_Delgado3'>ResearchGate</a></p>");
 //document.write("<p><a href='https://www.scopus.com/authid/detail.uri?authorId=35147720500'>Scopus</a></p>");
-document.write("<p><a href='http://www.researcherid.com/rid/W-3643-2018'>ResearcherID</a></p>");
+document.write("<p><a href='https://publons.com/researcher/1824953/ramon-a-delgado/'>Publons</a></p>");
 document.write("<p><a ");
 document.write("  href='http://scholar.google.com.au/citations?user=L9US5bgAAAAJ&hl=en'");
 document.write("  target='_blank'>Google Scholar</a></p>");
