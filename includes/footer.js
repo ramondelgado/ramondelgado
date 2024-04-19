81d543bfaf6de662ef1e3f8e5a21a26d4c0ef0e9
Ramon A. Delgado <ramon.dp@gmail.com>
2024-04-19 23:17:30 +0200

    Update publications
    Added J13 and C20 to the publication list

--- includes/footer.js
+++ includes/footer.js
@@ -1,3 +1,3 @@
 document.write("<div id='footer'>");
-document.write(" &copy  2020 <a href='mailto:ramon.delgado_at_uon.edu.au'>RADP</a>");
+document.write(" &copy  2024 RADP");
 document.write("</div>");
